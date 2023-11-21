6e7df51ca3b764eeceb779f76581418f678319ef
itskellyturner <itsturnerkelly@gmail.com>
2023-11-21 21:42:05 +0200

    new file:   components/Booking/Booking.css new file:   components/Booking/Booking.js 	new file:   components/Footer/Footer.css     #	new file:   components/Footer/Footer.js 	new file:   components/Header/Header.css 	new file:   components/Header/Header.js 	new file:   components/Home/Home.css 	new file:   components/Home/Home.js 	new file:   components/Menu/Menu.css 	new file:   components/Menu/Menu.js 	new file:   components/Nav.js 	new file:   components/Order/Order.css 	new file:   components/Order/Order.js 	modified:   src/App.js 	modified:   src/index.js 	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./components/Home/HomePage";
+import BookingPage from "./components/Booking/BookingPage";
+import OrderPage from "./components/Order/OrderPage";
+import { Routes, Route } from "react-router-dom";
+import MenuPage from "./components/Menu/MenuPage";
+import { AppProvider } from './components/context/AppContext';
+import Confirmation from "./components/Booking/Confirmation";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+        <img className="bg" alt="Site Background" src={require('../src/assets/images/table.JPG')}/>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/menu" element={<MenuPage />}></Route>
+            <Route path="/order" element={<OrderPage />}></Route>
+            <Route path="/book" element={<BookingPage />}></Route>
+            <Route path="/confirmation" element={<Confirmation />}></Route>
+          </Routes>
+        </div>
+    </AppProvider>
   );
-}
+};
 
 export default App;
